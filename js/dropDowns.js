bdf049d0af812a3a9b81f1d2b129cddcc80a1199
NeeKo3 <leshaxx007@gmail.com>
2023-11-14 15:48:32 +0300

    added comments

--- js/dropDowns.js
+++ js/dropDowns.js
@@ -1,5 +1,6 @@
 import { Dropdown } from "./dropDown.js";
 
+// This class is needed for cases where you add more than one dropdown.
 export class Dropdowns {
   constructor(dropDownSelector) {
     this.dropDowns = document.querySelectorAll(dropDownSelector);
